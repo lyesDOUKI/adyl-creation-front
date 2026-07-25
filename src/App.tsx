d89368d4cb39f1783037327bf9cf500497585c51
Lyes DOUKI <lyes.douki@gmail.com>
2026-07-25 19:11:34 +0200

    chore: add css + index starter

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CartProvider } from "@/ui/cart/CartContext";
+import Index from "./pages/Index";
+import ProductDetail from "./pages/ProductDetail";
+import Checkout from "./pages/Checkout";
+import OrderTracking from "./pages/OrderTracking";
+import OrderDetail from "./pages/OrderDetail";
+import Contact from "./pages/Contact";
+import Appointments from "./pages/Appointments";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <CartProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/produit/:id" element={<ProductDetail />} />
+            <Route path="/commande" element={<Checkout />} />
+            <Route path="/suivi" element={<OrderTracking />} />
+            <Route path="/suivi/:id" element={<OrderDetail />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/rendez-vous" element={<Appointments />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </CartProvider>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
