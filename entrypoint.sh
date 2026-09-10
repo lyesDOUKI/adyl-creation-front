73f7c36572ce93e89ea03684ccfcdcd19773ef92
Lyes DOUKI <lyes.douki@gmail.com>
2026-09-10 19:42:54 +0200

    fix: nginx read config file for env.js

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@ set -eu
 
 envsubst '${KEYCLOAK_URL} ${KEYCLOAK_REALM} ${KEYCLOAK_CLIENT_ID}' \
   < /usr/share/nginx/html/env.template.js \
-  > /usr/share/nginx/html/env.js
+  > /config/env.js
 
 exec nginx -g 'daemon off;'
