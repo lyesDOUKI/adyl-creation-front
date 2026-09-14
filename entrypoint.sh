61f8f2cd4321d06a0067fe5423b8dc748f83f510
Lyes DOUKI <lyes.douki@gmail.com>
2026-09-14 18:06:49 +0200

    fix: add api url in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-envsubst '${KEYCLOAK_URL} ${KEYCLOAK_REALM} ${KEYCLOAK_CLIENT_ID}' '${GEOCODE_API_URL}' \
+envsubst '${KEYCLOAK_URL} ${KEYCLOAK_REALM} ${KEYCLOAK_CLIENT_ID} ${GEOCODE_API_URL}' \
   < /usr/share/nginx/html/env.template.js \
   > /config/env.js
 
